c0031b73803990797081fd6f87bdd827d105d686
Wei Jing <jaychou123103@Hotmail.com>
2025-08-27 23:49:35 +1200

    add model and viewmodel

---
+++ WPF-Photo-Manager/WPF-Photo-Manager/Models/Photo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+/// <summary>
+/// Represents a photo or video file in the application.
+/// This model class holds the core data for a media item.
+/// </summary>
+public class Photo
+{
+    /// <summary>
+    /// Gets or sets the full file path of the photo.
+    /// </summary>
+    public string FilePath { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the file (e.g., "IMG_1234.jpg").
+    /// </summary>
+    public string FileName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time the file was created.
+    /// </summary>
+    public DateTime CreationDate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the file path to the generated thumbnail image.
+    /// This is used for quick UI display.
+    /// </summary>
+    public string ThumbnailPath { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the Photo class.
+    /// </summary>
+    /// <param name="filePath">The full path to the media file.</param>
+    public Photo(string filePath)
+    {
+        FilePath = filePath;
+        // Use FileInfo to get file properties without needing to handle
+        // the file itself, making this class efficient.
+        try
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            FileName = fileInfo.Name;
+            CreationDate = fileInfo.CreationTime;
+        }
+        catch (Exception ex)
+        {
+            // Log the error but proceed with default values to prevent app crash.
+            Console.WriteLine($"Error initializing Photo model for file {filePath}: {ex.Message}");
+            FileName = Path.GetFileName(filePath);
+            CreationDate = DateTime.MinValue;
+        }
+    }
+}
---
+++ WPF-Photo-Manager/WPF-Photo-Manager/ViewModels/PhotoViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+/// <summary>
+/// A ViewModel class that wraps the Photo model.
+/// This class handles UI-related logic and implements INotifyPropertyChanged
+/// to notify the UI of property changes, enabling data binding.
+/// </summary>
+public class PhotoViewModel : INotifyPropertyChanged
+{
+    private readonly Photo _photo;
+    private BitmapImage _thumbnail;
+
+    /// <summary>
+    /// Event that fires when a property value changes.
+    /// This is a key part of the MVVM pattern for data binding.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// Gets the full file path from the underlying Photo model.
+    /// </summary>
+    public string FilePath => _photo.FilePath;
+
+    /// <summary>
+    /// Gets the file name from the underlying Photo model.
+    /// </summary>
+    public string FileName => _photo.FileName;
+
+    /// <summary>
+    /// Gets the thumbnail image to be displayed in the UI.
+    /// The property setter notifies the UI when the thumbnail is loaded.
+    /// </summary>
+    public BitmapImage Thumbnail
+    {
+        get => _thumbnail;
+        set
+        {
+            if (_thumbnail != value)
+            {
+                _thumbnail = value;
+                OnPropertyChanged(nameof(Thumbnail));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the PhotoViewModel class.
+    /// </summary>
+    /// <param name="photo">The Photo model instance to wrap.</param>
+    public PhotoViewModel(Photo photo)
+    {
+        _photo = photo;
+        // The thumbnail itself will be set later by the file scanning service,
+        // which will trigger the 'set' block and notify the UI.
+    }
+
+    /// <summary>
+    /// Notifies listeners that a property value has changed.
+    /// </summary>
+    /// <param name="propertyName">The name of the property that changed.</param>
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
